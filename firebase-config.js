a5f98248ebb289ea612fd59829f31fb275545377
Dakshit2012 <ritikajainmzn@gmail.com>
2025-07-01 17:41:37 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+<!-- firebase-config.js -->
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyC_ehw0esOn3pnioM8OsRQBvVDnxZPGJms",
+    authDomain: "ultimate-chess-882ef.firebaseapp.com",
+    databaseURL: "https://ultimate-chess-882ef-default-rtdb.firebaseio.com",
+    projectId: "ultimate-chess-882ef",
+    storageBucket: "ultimate-chess-882ef.appspot.com",
+    messagingSenderId: "1053103885430",
+    appId: "1:1053103885430:web:645bf6eca13f533083a357",
+    measurementId: "G-VL0YV37W7W"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
